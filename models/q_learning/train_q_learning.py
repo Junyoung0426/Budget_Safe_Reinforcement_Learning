5c1760643d21b2c17186b236722bbc1f7c23a29f
Junyoung Son <sjy990426@gmail.com>
2024-12-03 17:04:12 +0900

    refactor: change the num of episode

--- models/q_learning/train_q_learning.py
+++ models/q_learning/train_q_learning.py
@@ -6,7 +6,7 @@ env = gym.make('Blackjack-v1')
 
 agent = QLearningAgent(env, learning_rate=0.1, discount_factor=0.99, epsilon_start=1.0, epsilon_min=0.01, epsilon_decay=0.999995, initial_balance=1000000, bet_amount=100)
 
-agent.train(num_episodes=500)
+agent.train(num_episodes=50000)
 
 compare_dir = Path(__file__).resolve().parent.parent.parent  / "compare" / "action_file"
 compare_dir.mkdir(exist_ok=True) 

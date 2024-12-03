bb9e81996fe36f74ee706331517f2e43ada3e82b
Junyoung Son <sjy990426@gmail.com>
2024-12-03 17:02:30 +0900

    refactor: change the num of epsiode

--- models/ddqn/train_ddqn.py
+++ models/ddqn/train_ddqn.py
@@ -7,7 +7,7 @@ env = gym.make('Blackjack-v1')
 
 agent = DDQNAgent(env, learning_rate=0.01, discount_factor=0.99, epsilon_start=1.0, epsilon_end=0.01, epsilon_decay=0.995, batch_size=64, memory_size=10000, bet_amount=100, initial_money=10000000)
 
-agent.train(num_episodes=10000)
+agent.train(num_episodes=500000)
 
 compare_dir = Path(__file__).resolve().parent.parent.parent  / "compare" / "action_file"
 compare_dir.mkdir(exist_ok=True) 

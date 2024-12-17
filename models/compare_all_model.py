03efb83a3f09dcda393eb088ea3f7134d5a86c4f
Junyoung Son <sjy990426@gmail.com>
2024-12-17 17:31:47 +0900

    refactor: chang the parameter

--- models/compare_all_model.py
+++ models/compare_all_model.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 env = gym.make('Blackjack-v1')
 
-qagent = QLearningAgent(env, learning_rate=0.01, discount_factor=0.99, epsilon_start=1.0, epsilon_min=0.01, epsilon_decay=0.995, initial_balance=1000000, bet_amount=1000)
+qagent = QLearningAgent(env, learning_rate=0.01, discount_factor=0.99, epsilon_start=1.0, epsilon_min=0.01, epsilon_decay=0.9995, initial_balance=1000000, bet_amount=1000)
 qagent.train(num_episodes=50000)
 
 ucb_agent =  UCBQLearningAgent(
@@ -21,10 +21,10 @@ ucb_agent =  UCBQLearningAgent(
 )
 ucb_agent.train(num_episodes=50000)
 
-dqnagent = DQNAgent(env, learning_rate=0.01, discount_factor=0.99, epsilon_start=1.0, epsilon_end=0.01, epsilon_decay=0.995, batch_size=64, memory_size=10000, bet_amount=1000, initial_money=1000000)
+dqnagent = DQNAgent(env, learning_rate=0.01, discount_factor=0.99, epsilon_start=1.0, epsilon_end=0.01, epsilon_decay=0.9995, batch_size=64, memory_size=10000, bet_amount=1000, initial_money=1000000)
 dqnagent.train(num_episodes=50000)
 
-ddqnagent = DDQNAgent(env, learning_rate=0.01, discount_factor=0.99, epsilon_start=1.0, epsilon_end=0.01, epsilon_decay=0.995, batch_size=64, memory_size=10000, bet_amount=1000, initial_money=1000000)
+ddqnagent = DDQNAgent(env, learning_rate=0.01, discount_factor=0.99, epsilon_start=1.0, epsilon_end=0.01, epsilon_decay=0.9995, batch_size=64, memory_size=10000, bet_amount=1000, initial_money=1000000)
 ddqnagent.train(num_episodes=50000)
 
 
